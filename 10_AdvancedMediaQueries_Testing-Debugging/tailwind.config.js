97e062b2b6500c4103eb8fd7e2942903084d974a
smjoy222 <smjoy222@gmail.com>
2026-05-12 23:34:35 +0600

    ResponsiveTableDesign

---
+++ 10_AdvancedMediaQueries_Testing-Debugging/tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                warning: "#e60b0b",
+                band: "#e60b99",
+            },
+            fontFamily: {
+                popopins: ['Poppins', 'sans-serif'],
+                playwrite: ["Playwrite Argentina", "cursive"],
+            },
+            screens: {
+                mobile: '480px',
+                tab: '768px',
+                desktop: '1024px',
+            },
+        }
+    }
+}
+
